aaf1ff62c5b76a94237debe25d0c24ab68fd5419
Arjun <arjunsohur@gmail.com>
2023-07-01 21:37:48 -0400

    Self Attention

--- multi_head_attention.py
+++ multi_head_attention.py
@@ -5,11 +5,13 @@ from self_attention import SelfAttentionHead
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, number_of_heads: int, input_dimension: int, queries_keys_dimension: int, values_dimension: int):
+    def __init__(self, number_of_heads: int, embedding_dimension: int, queries_keys_hidden_dimension: int,
+                 values_hidden_dimension: int):
         super(MultiHeadAttention, self).__init__()
-        self.heads = nn.ModuleList([SelfAttentionHead(input_dimension, queries_keys_dimension, values_dimension)
+        self.heads = nn.ModuleList([SelfAttentionHead(embedding_dimension, queries_keys_hidden_dimension,
+                                                      values_hidden_dimension)
                                     for _ in range(number_of_heads)])
-        self.feed_forward_layer = nn.Linear(number_of_heads*values_dimension, input_dimension)
+        self.feed_forward_layer = nn.Linear(number_of_heads * values_hidden_dimension, embedding_dimension)
 
     def forward(self, query: Tensor, key: Tensor, value: Tensor):
         multi_head_result = torch.cat([head(query, key, value) for head in self.heads], dim=-1)
